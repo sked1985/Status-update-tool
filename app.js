8f5ae96e2d9709868a635026d0e8ed34b6f594e7
sked1985 <mcgettiganbarry@yahoo.com>
2014-12-19 22:21:15 +0000

    Status update tool
    I created this status update tool using Jquery and html

---
+++ app.js
@@ -0,0 +1,17 @@
+var main = function(){
+ $('.btn').click(function() {  
+    var post = $('.status-box').val();
+    $('<li>').text(post).prependTo('.posts');
+    $('.status-box').val('');
+    $('.counter').text(140);
+ });
+ 
+ $('.status-box').keyup(function(){
+ var postLength = $(this).val().length;     
+ var charactersLeft = 140-postLength;
+ $('.counter').text(charactersLeft);
+ 
+});
+}
+
+$(document).ready(main);
